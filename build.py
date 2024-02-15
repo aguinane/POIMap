b893bcbbbe24ab1454a1f7b03bf8610692cf8754
Alex Guinman <alex@guinman.id.au>
2024-02-15 15:12:09 +1000

    Multiple configs

--- build.py
+++ build.py
@@ -1,15 +1,26 @@
-import toml
+import tomli
 from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
 
 env = Environment(loader=FileSystemLoader(""))
 
 template = env.get_template("template.html")
 
-with open('config.toml', 'r') as fh:
-    page_data = toml.loads(fh.read())
 
-output_html = template.render(**page_data)
-file_path = 'output.html'
-with open(file_path, "w", encoding="utf-8") as fh:
-    fh.write(output_html)
-print("Created", file_path)
+config_dir = Path('configs')
+config_dir.mkdir(exist_ok=True)
+output_dir = Path('output')
+output_dir.mkdir(exist_ok=True)
+
+
+for fp in config_dir.glob("*.toml"):
+
+    name = fp.stem
+    with open(fp, 'r') as fh:
+        page_data = tomli.loads(fh.read())
+
+    output_html = template.render(**page_data)
+    output_path = output_dir / f'{name}.html'
+    with open(output_path, "w", encoding="utf-8") as fh:
+        fh.write(output_html)
+    print("Created", output_path)
